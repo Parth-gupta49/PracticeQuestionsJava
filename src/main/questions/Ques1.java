96d9376cadccd810c2ae4647630bba3542ffec30
Parth-gupta49 <nbparthgupta4959@gmail.com>
2023-10-02 10:51:09 +0530

    Initial commit

---
+++ src/main/questions/Ques1.java
@@ -0,0 +1,8 @@
+package main.questions;
+
+public class Ques1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Heey");
+    }
+}
